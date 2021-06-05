61cd7df4012bc0d600a98a138f95823fba219654
superLipbalm <superLipbalm@gmail.com>
2021-06-05 23:59:13 +0900

    CategoryList 컴포넌트, Gatsby Link API의 Link 컴포넌트 기반 CategoryItem 컴포넌트를 추가하여 카테고리 클릭 시 해당 주소로 이동하는 기능 및 선택된 카테고리 강조 표시 기능 구현.

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
-import React from 'react';
+import { Link } from 'gatsby';
+import React, { ReactNode } from 'react';
 
 export interface CategoryListProps {
   selectedCategory: string;
@@ -8,20 +9,55 @@ export interface CategoryListProps {
   };
 }
 
+type CategoryItemProps = {
+  active: boolean;
+};
+
+type GatsbyLinkProps = {
+  children: ReactNode;
+  className?: string;
+  to: string;
+} & CategoryItemProps;
+
 const CategoryListWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  width: 760px;
+  width: 768px;
   margin: 100px auto 0;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-top: 50px;
+    padding: 0 20px;
+  }
 `;
 
+const CategoryItem = styled(({ active, to, ...props }: GatsbyLinkProps) => (
+  <Link to={to} {...props} />
+))`
+  margin-right: 20px;
+  padding: 5px 0;
+  font-size: 18px;
+  font-weight: ${({ active }) => (active ? '800' : '400')};
+  cursor: pointer;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function CategoryList({ selectedCategory, categoryList }: CategoryListProps) {
   return (
     <CategoryListWrapper>
       {Object.entries(categoryList).map(([name, count]) => (
-        <div key={name}>
+        <CategoryItem
+          to={`/?category=${name}`}
+          key={name}
+          active={name === selectedCategory}
+        >
           #{name}({count})
-        </div>
+        </CategoryItem>
       ))}
     </CategoryListWrapper>
   );
